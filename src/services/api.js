42b20223a85a3e88a43e17b35f445d1ab7c1af33
Rejane Raquel <rejane.lima87@gmail.com>
2022-07-07 17:06:06 -0300

    Implementa Função getCategories

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,17 @@
 export async function getCategories() {
-  // Implemente aqui - teste!
+  //try {
+  const returnAPI = await fetch("https://api.mercadolibre.com/sites/MLB/categories");
+  const returnAPIJson = await returnAPI.json();
+  console.log(returnAPIJson);
+  return returnAPIJson;
+  // console.log(returnAPI);
+  /* } catch (error) {
+    // return Error
+    console.log(`Algo deu errado \n${error}`)
+  } */
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe 
-}
+  export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+    // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe 
+  }
 

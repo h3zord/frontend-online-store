df14458def417a5e22ddb5c3e85e1ad9ec235cfd
Rejane Raquel <rejane.lima87@gmail.com>
2022-07-07 16:13:41 -0300

    Inciando Projeto

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 export async function getCategories() {
-  // Implemente aqui
+  // Implemente aqui - teste!
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe 
 }
+

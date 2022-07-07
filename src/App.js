c1bf8391299768f2de537fb4d835604bb706aec3
Rejane Raquel <rejane.lima87@gmail.com>
2022-07-07 17:27:48 -0300

    Req 01 -Correções Lint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import './App.css';
-import { getCategories } from './services/api';
 
 class App extends React.Component {
-  render(){
-    getCategories()
-  return (
-    <>
-      <h1>Group 24</h1>
-      
-    </>
-  );
+  render() {
+    return (<h1>Group 24</h1>);
   }
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
-  const returnAPI = await fetch("https://api.mercadolibre.com/sites/MLB/categories");
+  const returnAPI = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const returnAPIJson = await returnAPI.json();
   return returnAPIJson;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const returnAPI = await fetch(url);
   const returnAPIJson = await returnAPI.json();
   return returnAPIJson;

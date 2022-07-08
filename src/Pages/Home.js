4291d57ae850d53f0eb1a251e93b8ae4c21004ba
Juliana Alvares <alvares.juliana@hotmail.com>
2022-07-08 11:52:38 -0300

    req-04: add buttons for product categories in Home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,28 +1,78 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CartButton from './Components/CartButton';
+import { getCategories } from '../services/api';
+import Loading from './Components/Loading';
+import '../Styles/homeStyle.css';
 
 class Home extends React.Component {
   constructor() {
     super();
+    this.state = {
+      productSearch: false,
+      productCategories: [],
+      isLoading: false,
+    };
+  }
 
-    this.state = { productSearch: false };
+  componentDidMount() {
+    this.getProductCategories();
+  }
+
+  getProductCategories = () => {
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      const categoryList = await getCategories();
+      this.setState({
+        productCategories: categoryList,
+        isLoading: false,
+      });
+    });
+  }
+
+  getCategoryBtns = (productCategories) => {
+    const categoryBtns = productCategories.map(({ id, name }) => (
+      <button
+        key={ id }
+        type="button"
+        data-testid="category"
+        onClick={ () => {} }
+      >
+        {name}
+      </button>
+    ));
+
+    return categoryBtns;
   }
 
   render() {
-    const { productSearch } = this.state;
+    const { productSearch, isLoading, productCategories } = this.state;
     const { history } = this.props;
 
-    if (!productSearch) {
-      return (
-        <>
-          <h1 data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </h1>
-          <CartButton history={ history } />
-        </>
-      );
-    }
+    return (
+      <div className="home-page">
+        <aside className="category-list">
+          {
+            isLoading ? <Loading /> : this.getCategoryBtns(productCategories)
+          }
+        </aside>
+        <main>
+          <div className="search-and-cart">
+            <CartButton history={ history } />
+          </div>
+          <section className="searched-products">
+            {
+              !productSearch && (
+                <h1 data-testid="home-initial-message">
+                  Digite algum termo de pesquisa ou escolha uma categoria.
+                </h1>
+              )
+            }
+          </section>
+        </main>
+      </div>
+    );
   }
 }
 

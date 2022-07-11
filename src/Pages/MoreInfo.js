58976339409972369bdbefa5ab3ce49b81547886
Rejane Raquel <rejane.lima87@gmail.com>
2022-07-11 17:04:29 -0300

    Req 11 - Create evaluation form

--- src/Pages/MoreInfo.js
+++ src/Pages/MoreInfo.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import { getProductFromProductId } from '../services/api';
-import CartButton from './Components/CartButton';
-import '../Styles/homeStyle.css';
 import { addProductInCart } from '../services/cartList';
+import '../Styles/homeStyle.css';
+import CartButton from './Components/CartButton';
 
-class MoreInfo extends Component {
+class MoreInfo extends React.Component {
   constructor() {
     super();
 
     this.state = {
       productInfo: {},
+      emailInput: '',
+      textAreaInput: '',
+      rateButton: 0,
     };
   }
 
   componentDidMount() {
     this.getProductInfo();
+  }
+
+  handleRateChange = (rate) => {
+    this.setState({ rateButton: rate });
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
   }
 
   getProductInfo = async () => {
@@ -27,9 +39,22 @@ class MoreInfo extends Component {
 
   render() {
     const {
-      productInfo: { thumbnail, price, title, warranty, id },
+      productInfo: { thumbnail, price, title, warranty, id }, emailInput, textAreaInput,
     } = this.state;
     const { history } = this.props;
+    const rateButtons = [];
+    const maxRate = 5;
+    for (let i = 1; i <= maxRate; i += 1) {
+      rateButtons.push(
+        <button
+          onClick={ () => this.handleRateChange(i) }
+          data-testid={ `${i}-rating` }
+          type="button"
+        >
+          {i}
+        </button>,
+      );
+    }
     return (
       <div className="info-container">
         <CartButton history={ history } />
@@ -37,10 +62,10 @@ class MoreInfo extends Component {
           <img src={ thumbnail } alt="imagem ilustrativa do produto" />
         </div>
         <div>
-          <p data-testid="product-detail-name">{ title }</p>
-          <p>{ id }</p>
-          <p>{ price }</p>
-          <p>{ warranty }</p>
+          <p data-testid="product-detail-name">{title}</p>
+          <p>{id}</p>
+          <p>{price}</p>
+          <p>{warranty}</p>
         </div>
         <button
           type="button"
@@ -49,6 +74,33 @@ class MoreInfo extends Component {
         >
           Adicionar ao Carrinho
         </button>
+        <form>
+          <h1>Avaliação</h1>
+          <input
+            onChange={ this.handleChange }
+            type="email"
+            name="emailInput"
+            value={ emailInput }
+            data-testid="product-detail-email"
+            placeholder="Digite seu e-mail"
+          />
+          <div>
+            {rateButtons}
+          </div>
+          <textarea
+            onChange={ this.handleChange }
+            data-testid="product-detail-evaluation"
+            name="textAreaInput"
+            value={ textAreaInput }
+            placeholder="Adicione seus comentários sobre o produto (opcional)"
+          />
+          <button
+            type="submit"
+            data-testid="submit-review-btn"
+          >
+            Avaliar
+          </button>
+        </form>
       </div>
     );
   }

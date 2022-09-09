cf2575855433a0c40082c113c761ccdb7acc0e00
Lucas <lucaschmain@gmail.com>
2022-09-09 13:34:45 -0300

    Updates

---
+++ precache-manifest.9e065d35bc6e1832399289a89fc3fa87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4043ebe337786926f82d9bea7d495dc3",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "84a594791ac938f4ecf3",
+    "url": "/frontend-online-store/static/css/main.39ddeecc.chunk.css"
+  },
+  {
+    "revision": "2448bd0f5e7b5f0c1fc0",
+    "url": "/frontend-online-store/static/js/2.9f8c54fd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.9f8c54fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a594791ac938f4ecf3",
+    "url": "/frontend-online-store/static/js/main.7d6ff767.chunk.js"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  }
+]);

a1af494e80184c64c55d9afa3a8c290076c23f02
Lucas <lucaschmain@gmail.com>
2023-01-15 21:35:59 -0300

    Updates

---
+++ precache-manifest.6f2fc67a6c30291e6670efb74accedb2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f80fbeb16e28d65c377f559b2fa873a",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "c7df600555ea1515c807",
+    "url": "/frontend-online-store/static/css/main.f99a55c6.chunk.css"
+  },
+  {
+    "revision": "9295d1d479d3b6089f39",
+    "url": "/frontend-online-store/static/js/2.fdee0109.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.fdee0109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7df600555ea1515c807",
+    "url": "/frontend-online-store/static/js/main.6d6918d0.chunk.js"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  },
+  {
+    "revision": "ecd17b03a104984c3c5a4c6e10b3f2a5",
+    "url": "/frontend-online-store/static/media/boleto.ecd17b03.svg"
+  },
+  {
+    "revision": "cbdf2d6f1fd22a13b794c60c3af0adac",
+    "url": "/frontend-online-store/static/media/elo.cbdf2d6f.svg"
+  },
+  {
+    "revision": "bb8b39dc119dcee373969161f45e3308",
+    "url": "/frontend-online-store/static/media/emptyCart.bb8b39dc.svg"
+  },
+  {
+    "revision": "2968255e1da9fd5d5c2792465c691c57",
+    "url": "/frontend-online-store/static/media/logo.2968255e.svg"
+  },
+  {
+    "revision": "8bae4642c53afa73cd4d4eea7f241177",
+    "url": "/frontend-online-store/static/media/magnifier.8bae4642.svg"
+  },
+  {
+    "revision": "34b3f074e4e55d286e797329f18cf916",
+    "url": "/frontend-online-store/static/media/mastercard.34b3f074.svg"
+  },
+  {
+    "revision": "9324c81c38b5ee2a27488240df32bcc6",
+    "url": "/frontend-online-store/static/media/star.9324c81c.svg"
+  },
+  {
+    "revision": "9273d597afe7ff9f90425cbb4f3161a5",
+    "url": "/frontend-online-store/static/media/sub.9273d597.svg"
+  },
+  {
+    "revision": "12c9569e2b58dfe61a22cdf9d7e2f49c",
+    "url": "/frontend-online-store/static/media/sum.12c9569e.svg"
+  },
+  {
+    "revision": "87c6057f885971e8be84c4377dbb6574",
+    "url": "/frontend-online-store/static/media/text.87c6057f.svg"
+  },
+  {
+    "revision": "3fc54b26a72ea028f9e276d79bef5a1e",
+    "url": "/frontend-online-store/static/media/text2.3fc54b26.svg"
+  },
+  {
+    "revision": "a42fe1b71bb7e0d1cff7f0d8382aa075",
+    "url": "/frontend-online-store/static/media/visa.a42fe1b7.svg"
+  },
+  {
+    "revision": "6b1c45c7a00085f2c55642173fdbe00b",
+    "url": "/frontend-online-store/static/media/voltar.6b1c45c7.svg"
+  }
+]);
